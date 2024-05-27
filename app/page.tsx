5f3b043f0d87d6b0ecde46e8a564ff1afe2b456c
Sion Kim <94289633+mandlake@users.noreply.github.com>
2024-05-27 14:48:48 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+function Home() {
+  return (
+    <>
+      <div className="text-blue-400 flex flex-col justify-center items-center w-screen mt-10">
+        <h1 className="font-semibold text-[28px] mb-10">Menus Examples</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ redux/reducer.ts
@@ -0,0 +1,24 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+
+const createNoopStorage = () => {
+  return {
+    getItem() {
+      return Promise.resolve(null);
+    },
+    setItem(_key: string, value: number) {
+      return Promise.resolve(value);
+    },
+    removeItem() {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage =
+  typeof window !== "undefined"
+    ? createWebStorage("local")
+    : createNoopStorage();
+
+export const rootReducer = combineReducers({});
